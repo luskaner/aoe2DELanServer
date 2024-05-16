7fea74d8f4e817a9c4488de739ca2991a7bfd624
David Fernández Aldana <luskaner@gmail.com>
2024-05-16 22:43:45 +0200

    Added --force option to server/genCert, simplified Taskfile(s) thus made faster and now multiple debug targets can be run at the same time

--- server/genCert/main.go
+++ server/genCert/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"common"
+	"flag"
 	"genCert/internal"
 	"log"
 	"os"
 )
 
+type Arguments struct {
+	Force bool
+}
+
 func main() {
 	log.SetFlags(0)
+	args := Arguments{}
+	flag.BoolVar(&args.Force, "force", false, "Whether to overwrite existing certificate pair")
+	flag.Parse()
 	certificatePairFolder := common.CertificatePairFolder(os.Args[0])
 	if certificatePairFolder == "" {
 		log.Fatal("Failed to determine certificate pair folder")
 	}
-	if common.HasCertificatePair(os.Args[0]) {
-		log.Fatal("Already have certificate pair, delete it manually and re-run.")
+	if !args.Force && common.HasCertificatePair(os.Args[0]) {
+		log.Fatal("Already have certificate pair and force is false, set force to true or delete it manually.")
 	}
 	if !internal.GenerateCertificatePair(certificatePairFolder) {
 		log.Fatal("Could not generate certificate pair.")
