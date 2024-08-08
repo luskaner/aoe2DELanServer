10b8a40c4f3a75555e84fc1eb6e5e34f67baaafc
David Fernández Aldana <luskaner@gmail.com>
2024-08-08 21:17:46 +0200

    Server: fixed game/party/reportMatch stub

--- server/internal/routes/game/party/reportMatch.go
+++ server/internal/routes/game/party/reportMatch.go
@@ -6,5 +6,5 @@ import (
 )
 
 func ReportMatch(w http.ResponseWriter, _ *http.Request) {
-	i.JSON(&w, i.A{2, i.A{}, i.A{}, i.A{}, nil, i.A{}, i.A{}, i.A{}, i.A{}, i.A{}, i.A{}, 0, nil, i.A{}, i.A{}, i.A{}})
+	i.JSON(&w, i.A{2, i.A{}, i.A{0, "", "", "", 0, ""}, i.A{-1, "", "", "", 0, ""}, nil, i.A{}, i.A{}, i.A{}, i.A{}, i.A{}, i.A{}, 0, nil, i.A{}, i.A{}, i.A{}})
 }
