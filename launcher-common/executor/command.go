76301384a25b02b1518c31aa3ef7f7b0f0ed951d
David Fernández Aldana <luskaner@gmail.com>
2024-08-15 01:55:49 +0200

    Launcher: Updated launcher-common module to run an arbitrary command.

--- launcher-common/executor/command.go
+++ launcher-common/executor/command.go
@@ -1,1 +1,20 @@
 package executor
+
+import "golang.org/x/sys/windows"
+
+func RunCommand(cmd []string) (err error) {
+	var args []string
+	if len(cmd) > 1 {
+		args = cmd[1:]
+	}
+	result := ExecOptions{
+		File:           cmd[0],
+		WindowState:    windows.SW_NORMAL,
+		SpecialFile:    true,
+		Shell:          true,
+		UseWorkingPath: true,
+		Args:           args,
+	}.Exec()
+	err = result.Err
+	return
+}
