ecae591e7219851b8c10208be6246878b1b8196c
David Fernández Aldana <luskaner@gmail.com>
2024-09-11 06:36:26 +0200

    Launcher: launcher-agent updated related to launcher-common change.

--- launcher-agent/internal/config.go
+++ launcher-agent/internal/config.go
@@ -3,11 +3,11 @@ package internal
 import (
 	"github.com/luskaner/aoe2DELanServer/common"
 	launcherCommon "github.com/luskaner/aoe2DELanServer/launcher-common"
-	"github.com/luskaner/aoe2DELanServer/launcher-common/executor"
+	"github.com/luskaner/aoe2DELanServer/launcher-common/executor/exec"
 )
 
 func RunConfig(revertFlags []string) {
 	args := []string{launcherCommon.ConfigRevertCmd}
 	args = append(args, revertFlags...)
-	executor.ExecOptions{File: common.GetExeFileName(true, common.LauncherConfig), ExitCode: true, Wait: true, Args: args}.Exec()
+	exec.Options{File: common.GetExeFileName(true, common.LauncherConfig), ExitCode: true, Wait: true, Args: args}.Exec()
 }
